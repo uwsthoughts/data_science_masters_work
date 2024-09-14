392d8f96d98ddcec27a96ae15258139c8ec8cae7
uwsthoughts <168541312+uwsthoughts@users.noreply.github.com>
2024-09-14 00:42:19 -0400

    what a life

---
+++ 2024_Fall/data_607_data_management/week_three/fruits_after.R
@@ -0,0 +1,3 @@
+c("bell pepper", "bilberry", "blackberry", "blood orange", "blueberry", 
+"cantaloupe", "chili pepper", "cloudberry", "elderberry", "lime", 
+"lychee", "mulberry", "olive", "salal berry")
